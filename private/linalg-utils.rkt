4e2ed10d3b566b62a01ff3e2220b7aba21b8ef13
Rodrigo Setti <rodrigosetti@gmail.com>
2016-02-24 15:20:53 -0800

    Migrate linalg-utils module to typed/racket

--- private/linalg-utils.rkt
+++ private/linalg-utils.rkt
@@ -1,4 +1,4 @@
-#lang racket/base
+#lang typed/racket/base
 
 ;; Linear algebra combinators
 
@@ -6,16 +6,19 @@
 
 (provide (all-defined-out))
 
+(: rotation-matrix (-> Real (Matrix Real)))
 (define (rotation-matrix theta)
   (matrix [[(cos theta) (- (sin theta)) 0]
            [(sin theta) (cos theta)     0]
            [0           0               1]]))
 
+(: scaling-matrix (-> Real Real (Matrix Real)))
 (define (scaling-matrix sx sy)
   (matrix [[sx 0  0]
            [0  sy 0]
            [0  0  1]]))
 
+(: translation-matrix (-> Real Real (Matrix Real)))
 (define (translation-matrix dx dy)
   (matrix [[1 0 dx]
            [0 1 dy]
