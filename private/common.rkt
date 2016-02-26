542ef8a207888564fdd32e6aa4fe88f3238aca26
Rodrigo Setti <rodrigosetti@gmail.com>
2016-02-26 10:58:07 -0800

    Forget to commit this file

---
+++ private/common.rkt
@@ -0,0 +1,10 @@
+#lang typed/racket/base
+
+(provide Dc<%>)
+
+(define-type Dc<%> (Class [set-brush (-> Any Symbol Void)]
+                          [draw-polygon (-> (Listof (Pairof Number Number)) Void)]
+                          [draw-path (-> (Object) Void)]
+                          [set-pen (-> String Number Symbol Void)]
+                          [get-size (-> (Values Real Real))]
+                          [set-smoothing (-> Symbol Void)]))
